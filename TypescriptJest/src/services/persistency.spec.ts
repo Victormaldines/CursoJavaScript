20345d02f5e85bb5a549f0783b8d7a66a4cf1afb
Victor Maldines <victormaldines@gmail.com>
2022-09-01 21:34:11 -0300

    teste de comportamento

--- TypescriptJest/src/services/persistency.spec.ts
+++ TypescriptJest/src/services/persistency.spec.ts
@@ -1,13 +1,54 @@
-describe('TESTING SOMETHING (IT)', () => {
-  it('should return one', () => {
-    const number = 1;
-    expect(number).toBe(1);
-  });
-});
+import { Persistency } from './persistency';
 
-describe('TESTING SOMETHING (TEST)', () => {
-  test('should return Luiz', () => {
-    const name = 'Luiz';
-    expect(name).toBe('Luiz');
+describe('Persistency', () => {
+  /**
+   * os mocks (sut, consoleSpy) armazenam as informações de toHaveBeenCalledTimes() para os outros testes
+   * geralmente, gerando erros.
+   *
+   * afterEach() ~> Executa a função para cada vez que um teste (it(), test()) for chamado
+   *
+   * () => jest.clearAllMocks() ~> função anônima que chama jest.clearAllMocks()
+   *  jest.clearAllMocks() ~> "reseta" as infomações dos mocks para poder reinstanciá-las em testes subsequentes
+   *  reseta, por exemplo, a toHaveBeenCalledTimes()
+   */
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // test pattern:
+    // class que está sendo testada as 'sut' (System Under Test)
+    const sut = new Persistency();
+    expect(sut.saveOrder()).toBeUndefined;
+  });
+
+  // teste de comportamento (spyOn)
+  it('should call console.log once', () => {
+    const sut = new Persistency();
+    /**
+     * Jest vai observar o objeto com o método segundo do parâmetro
+     *  possível ver quantas vezes o método foi chamado (toHaveBeenCalledTimes(<qtdChamadas>: number))
+     * nomeVariavel = jest.spyOn(<objeto>: objetct, <metodo>: string): SpyInstance
+     */
+    // consoleSpy armazena o comportamento do método
+    const consoleSpy = jest.spyOn(console, 'log');
+
+    // chamando console.log() dentro do objeto sut
+    sut.saveOrder();
+
+    // console.log() precisa ser chamado 1 vez
+    expect(consoleSpy).toHaveBeenCalledTimes(1);
+  });
+
+  // testando comportamento com valor de chamada no parâmetro
+  it('should call console.log with "Order saved successfully"', () => {
+    const sut = new Persistency();
+    const consoleSpy = jest.spyOn(console, 'log');
+    sut.saveOrder();
+    /**
+     * toHaveBeenCalledWith(<parametro>: any)
+     * o teste passa caso a função testada for chamada com o valor passado no parâmetro
+     * ex: console.log('sla');
+     * expect(consoleSpy).toHaveBeenCalledWith('sla');
+     */
+    expect(consoleSpy).toHaveBeenCalledWith('Order saved successfully');
   });
 });
